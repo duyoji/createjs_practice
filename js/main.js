3ee338167753e4ef18014906279bc8f152c9ab05
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2013-06-28 22:28:49 +0900

    vector

--- js/main.js
+++ js/main.js
@@ -1,62 +1,74 @@
 (function (window) {
 
-    window.addEventListener('load', main);
+	window.addEventListener('load', main);
 
-    var stage;
-
-    // import
-	/*
-    function main(event) {
-        stage = new createjs.Stage('demoCanvas');
-        var circle = new createjs.Shape();
-        circle.graphics.beginFill('#00ffff').drawCircle(0, 0, 10);
-        circle.x = 100;
-        circle.y = 100;
-
-        createjs.Tween
-            .get(circle,{loop:true, useTick:true})
-            .to({x:400}, 3000)
-            .to({x:100}, 3000);
-
-        createjs.Ticker.addEventListener('tick',tick);
-
-
-        stage.addChild(circle);
-    }
-	*/
-
-	var angle = 0;
+	var stage;
+	var angle  = 60;
 	var circle = new createjs.Shape();
 	var radius = 30;
+	var speed  = 60;
 	createjs.Ticker.setFPS(60);
 
 	function main(event) {
-        stage = new createjs.Stage('demoCanvas');
-		stage.canvas.width  = window.innerWidth;
-		stage.canvas.height = window.innerHeight;
+			stage = new createjs.Stage('demoCanvas');
+			stage.canvas.width  = window.innerWidth;
+			stage.canvas.height = window.innerHeight;
 
-        circle.graphics.beginFill('#00ffff').drawCircle(0, 0, 10);
-        circle.x = stage.canvas.width / 2;
-        circle.y = stage.canvas.height / 2;
+			circle.graphics.beginFill('#00ffff').drawCircle(0, 0, 10);
+			circle.x = stage.canvas.width / 2;
+			circle.y = stage.canvas.height / 2;
 
-        createjs.Ticker.addEventListener('tick',tick);
+			createjs.Ticker.addEventListener('tick',tick);
 
 
-        stage.addChild(circle);
-    }
+			stage.addChild(circle);
+	}
 
-    function tick (event) {
-        stage.update();
-		angle += 30;
-		if (angle >= 360) {
-			angle = 0;
-		}
 
-		var x = Math.cos(angle * Math.PI/180) * radius + window.innerWidth / 2;
-		var y = Math.sin(angle * Math.PI/180) * radius + window.innerHeight / 2;
+	var xState     = 1;
+	var yState     = 1;
+	var speedState = 1;
 
-		circle.x = x;
-		circle.y = y;
-		
-    }
+	function tick (event) {
+			stage.update();
+			
+
+
+			speed += speedState;
+
+			if (speed < -100 || 100 < speed) {
+				speedState *= -1;
+			}
+
+
+			var radian = angle * Math.PI / 180;
+
+			var vx = Math.cos(radian) * speed * xState;
+			var vy = Math.sin(radian) * speed * yState;
+
+			console.log('vx, vy : ' + vx + ', ' + vy);
+			
+			circle.x += vx;
+			circle.y += vy;
+
+			if (circle.x  > stage.canvas.width || circle.x < 0) {
+				xState *= -1;
+			}
+			if (circle.y  > stage.canvas.height || circle.y < 0) {
+				yState *= -1;
+			}
+
+	/*
+	angle += 30;
+	if (angle >= 360) {
+		angle = 0;
+	}
+
+	var x = Math.cos(angle * Math.PI/180) * radius + window.innerWidth / 2;
+	var y = Math.sin(angle * Math.PI/180) * radius + window.innerHeight / 2;
+
+	circle.x = x;
+	circle.y = y;
+	*/	
+	}
 })(window);
